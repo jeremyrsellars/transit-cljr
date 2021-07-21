81eb7bc848e9e2b93af8dd53d272f4bf76e06e33
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2021-07-21 11:28:35 -0500

    TLC: Check only Apache2/copyright.
    Remove BigRational, etc. because it was MIT license and TryParse was CC-by-SA3 (from memory). Mixing licenses for very little benefit seemed unwise.

--- src/Transit.Tests/FactoryImplementationAdapter.cs
+++ src/Transit.Tests/FactoryImplementationAdapter.cs
@@ -1,9 +1,4 @@
-﻿// Modifications Copyright (C) 2021 Jeremy Sellars.
-// Copyright © 2014 Rick Beerendonk. All Rights Reserved.
-//
-// This code is a C# port of the Java version created and maintained by Cognitect, therefore
-//
-// Copyright © 2014 Cognitect. All Rights Reserved.
+﻿// Copyright (C) 2021 Jeremy Sellars.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
